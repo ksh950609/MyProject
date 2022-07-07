060653983fc05ca934d758486a33152511e2f705
ksh950609 <105829018+ksh950609@users.noreply.github.com>
2022-07-07 23:12:39 +0900

    Add files via upload

---
+++ src/main/webapp/bbs/Bbs.java
@@ -0,0 +1,50 @@
+package bbs;
+
+public class Bbs {
+
+	 private int bbsID;
+	 private String bbsTitle;
+	 private String userID;
+	 private String bbsDATE;
+	 private String bbsContent;
+	 private String bbsAvailable;
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getBbsTitle() {
+		return bbsTitle;
+	}
+	public void setBbsTitle(String bbsTitle) {
+		this.bbsTitle = bbsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getBbsDATE() {
+		return bbsDATE;
+	}
+	public void setBbsDATE(String bbsDATE) {
+		this.bbsDATE = bbsDATE;
+	}
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	public String getBbsAvailable() {
+		return bbsAvailable;
+	}
+	public void setBbsAvailable(String bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+
+	 
+	 
+}
